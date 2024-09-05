98d6355a092fe2b5bde36f31dade4275583489fb
bolunifemisimax <151167010+bolunifemisimax@users.noreply.github.com>
2024-09-05 12:18:34 +0100

    Larry project team 1
    Co-Authored-By: Banks Awesome  <111880603+banksawesome@users.noreply.github.com>
    Co-Authored-By: faith-fulness <146447864+faith-fulness@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Shop from './Shop'
 import Cart from './Cart'
+import Contact from './Contact'
+import Blog from './Blog'
+import Service from './Service'
+import About from './About'
 
 const App = () => {
   return (
@@ -11,6 +15,10 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/shop" element={<Shop />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/Blog" element={<Blog />} />
+        <Route path="/Services" element={<Service />} />
+        <Route path="/About" element={<About />} />
       </Routes>
     </Router>
   );
---
+++ src/Blog.jsx
@@ -0,0 +1,194 @@
+import React from 'react';
+import Navbar from './Navbar'
+import './assets/css/bootstrap.min.css';
+import './assets/css/style.css';
+import './assets/css/tiny-slider.css';
+const contact = () => {
+    return (
+        <>
+            <Navbar />
+            <div className="hero">
+                <div className="container">
+                    <div className="row justify-content-between">
+                        <div className="col-lg-5">
+                            <div className="intro-excerpt">
+                                <h1>Blog</h1>
+                                <p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique.</p>
+                                <p><a href="" className="btn btn-secondary me-2">Shop Now</a><a href="#" className="btn btn-white-outline">Explore</a></p>
+                            </div>
+                        </div>
+                        <div className="col-lg-7">
+                            <div className="hero-img-wrap">
+                                <img src="/src/assets/images/couch.png" className="img-fluid" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="blog-section">
+                <div className="container">
+
+                    <div className="row">
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-1.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">First Time Home Owner Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 19, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-2.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">How To Keep Your Furniture Clean</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Robert Fox</a></span> <span>on <a href="#">Dec 15, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-3.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">Small Space Furniture Apartment Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 12, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-1.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">First Time Home Owner Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 19, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-2.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">How To Keep Your Furniture Clean</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Robert Fox</a></span> <span>on <a href="#">Dec 15, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-3.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">Small Space Furniture Apartment Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 12, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-1.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">First Time Home Owner Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 19, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="/src/assets/images/post-2.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">How To Keep Your Furniture Clean</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Robert Fox</a></span> <span>on <a href="#">Dec 15, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-12 col-sm-6 col-md-4 mb-5">
+                            <div className="post-entry">
+                                <a href="#" className="post-thumbnail"><img src="src/assets/images/post-3.jpg" alt="Image" className="img-fluid" /></a>
+                                <div className="post-content-entry">
+                                    <h3><a href="#">Small Space Furniture Apartment Ideas</a></h3>
+                                    <div className="meta">
+                                        <span>by <a href="#">Kristin Watson</a></span> <span>on <a href="#">Dec 12, 2021</a></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <div class="testimonial-section before-footer-section" >
+                <div class="container" >
+                    <div class="row">
+                        <div class="col-lg-7 mx-auto text-center">
+                            <h2 class="section-title">Testimonials</h2>
+                        </div>
+                    </div>
+
+                    <div class="row justify-content-center" >
+                        <div class="col-lg-12" />
+                        <div class="testimonial-slider-wrap text-center" >
+
+                            <div id="testimonial-nav">
+                                <span class="prev" data-controls="prev"><span class="fa fa-chevron-left"></span></span>
+                                <span class="next" data-controls="next"><span class="fa fa-chevron-right"></span></span>
+                            </div>
+
+                            <div class="testimonial-slider">
+
+
+                                <div class="item">
+                                    <div class="row justify-content-center">
+                                        <div class="col-lg-8 mx-auto">
+
+                                            <div class="testimonial-block text-center">
+                                                <blockquote class="mb-5">
+                                                    <p>&ldquo;Donec facilisis quam ut purus rutrum lobortis. Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Integer convallis volutpat dui quis scelerisque.&rdquo;</p>
+                                                </blockquote>
+
+                                                <div class="author-info">
+                                                    <div class="author-pic">
+                                                        <img src="/src/assets/images/person-1.png" alt="Maria Jones" class="img-fluid" />
+                                                    </div>
+                                                    <h3 class="font-weight-bold">Maria Jones</h3>
+                                                    <span class="position d-block mb-3">CEO, Co-Founder, XYZ Inc.</span>
+                                                </div>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+}
+export default contact
